026600a635617e474e4504155223428f80f78c91
leandrosrlacerda <leandrolacerda.sistema@gmail.com>
2021-12-29 13:52:22 -0300

    criando class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -21,5 +21,41 @@ public class PrimeiroPrograma {
         Cachorro cachorro = new Cachorro("Sheik", "Preto", 2);
         System.out.println(cachorro);
 
+        Livro livro = new Livro("O problema dos 3 corpos", 300);
+        System.out.println("Livro: " + livro);
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
